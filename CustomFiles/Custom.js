02d50e7d956f629de302d60d8c9c9f0ef420d8cc
kelohin <kelohin@gmail.com>
2016-12-19 20:36:11 -0500

    ChangeRequests
    Add Online Ordering, Add Reservation #2, Modify about breakfast start
    time #1.

---
+++ CustomFiles/Custom.js
@@ -0,0 +1,38 @@
+﻿$(document).ready(function () {
+    //Init Page Load
+    $(document).ready(function () {
+        $('#content').load('Logo.html');
+    });
+    //Home func
+    $("#homeLink").click(function () {
+        $('#content').load('Logo.html');
+    });
+    //Our Story func
+    $("#ourStoryLink").click(function () {
+        $('#content').load('OurStory.html');
+    });
+    //Breakfast func
+    $("#breakfastLink").click(function () {
+        $('#content').load('Breakfast.html');
+    });
+    //Lunch func
+    $("#lunchLink").click(function () {
+        $('#content').load('Lunch.html');
+    });
+    //Dinner func
+    $("#dinnerLink").click(function () {
+        $('#content').load('Dinner.html');
+    });
+    //Gallery func
+    $("#galleryLink").click(function () {
+        $('#content').load('Gallery.html');
+    });
+    //Location func
+    $("#locationLink").click(function () {
+        $('#content').load('Location.html');
+    });
+    //About func
+    $("#aboutLink").click(function () {
+        $('#content').load('About.html');
+    });
+});
